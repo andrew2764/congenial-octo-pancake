22f628c40100b72401a6d6d0d7a480133c305576
andrew2764 <andrew2764@gmail.com>
2019-09-26 11:33:08 +0800

    This

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import random
 cont = True
 
-# import random
-# for x in range(10):
-#   print random.randint(1,101)
 while True:
     print random.randint(1,10)
     response = raw_input("Continue? (Y/N): ")
